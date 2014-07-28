d72444d8001551219b258dcae6f1f637bcaed8b0
alex <al.prokopenya@gmail.com>
2014-07-28 14:24:43 +0300

    Revert "test console app"
    This reverts commit 7f32eeeff6306e57870f53b90d79b0e0b50108dc.
    
    Conflicts:
    	InnaTourWebService/bin/InnaTourWebService.dll
    	InnaTourWebService/bin/InnaTourWebService.dll.CodeAnalysisLog.xml
    	InnaTourWebService/bin/InnaTourWebService.pdb
    	InnaTourWebService/obj/Debug/InnaTourWebService.dll
    	InnaTourWebService/obj/Debug/InnaTourWebService.pdb
    	ServiceTest/bin/Debug/ServiceTest.exe
    	ServiceTest/bin/Debug/ServiceTest.pdb
    	ServiceTest/obj/Debug/ServiceTest.csproj.FileListAbsolute.txt
    	ServiceTest/obj/Debug/ServiceTest.exe
    	ServiceTest/obj/Debug/ServiceTest.pdb

--- InnaTourWebService/BookService.asmx
+++ InnaTourWebService/BookService.asmx
@@ -1,1 +1,1 @@
-﻿<%@ WebService Language="C#" CodeBehind="~/App_Code/BookService.asmx.cs" Class="InnaTourWebService.BookService2" %>
+﻿<%@ WebService Language="C#" CodeBehind="BookService.asmx.cs" Class="InnaTourWebService.BookService" %>
--- InnaTourWebService/DataBase/DataBaseProvider.cs
+++ InnaTourWebService/DataBase/DataBaseProvider.cs
@@ -21,10 +21,29 @@ namespace InnaTourWebService.DataBase
         SqlConnection myConnection = null;
         #endregion
 
+
+        bool disposed = false;
+
+        // Public implementation of Dispose pattern callable by consumers. 
         public void Dispose()
         {
-            if ((this.myConnection.State != System.Data.ConnectionState.Broken) && (this.myConnection.State != System.Data.ConnectionState.Broken))
-                this.myConnection.Close();
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Protected implementation of Dispose pattern. 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                if ((this.myConnection.State != System.Data.ConnectionState.Broken) && (this.myConnection.State != System.Data.ConnectionState.Broken))
+                    this.myConnection.Close();
+            }
+
+            disposed = true;
         }
         
         #region public methods
